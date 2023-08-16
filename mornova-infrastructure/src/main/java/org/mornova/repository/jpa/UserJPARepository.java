aea210aca5b5f318caad085bfa8710da993e6f85
Theophane Kouchoanou <ptitkouche@yahoo.fr>
2023-08-16 23:17:41 +0200

    fetch user with role usig entity graph

--- mornova-infrastructure/src/main/java/org/mornova/repository/jpa/UserJPARepository.java
+++ mornova-infrastructure/src/main/java/org/mornova/repository/jpa/UserJPARepository.java
@@ -2,7 +2,13 @@ package org.mornova.repository.jpa;
 
 import org.mornova.domain.core.model.objectValue.ids.UserId;
 import org.mornova.repository.jpa.entity.UserJPA;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserJPARepository extends JpaRepository<UserJPA, UserId> {
+    @Override
+    @EntityGraph(attributePaths = "roles")
+    Optional<UserJPA> findById(UserId userId);
 }
